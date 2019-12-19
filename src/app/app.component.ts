7d7908e1954557ccebdc7064764ae2cea42cd8d5
denis.shtupa <denis.shtupa@3i-solutions.net>
2019-12-19 16:50:20 +0100

    player 1 player 2 overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'game-app';
+  public title = 'game-app';
+  public scores = [0, 0];
+  public enemySelected = -1;
+
+  private playerSelected = -1;
+  private loading = false;
+  private isResultShow = false;
+  private theResult = 0;
+  private weapons = [
+    'rock',
+    'paper',
+    'scissors'
+  ];
+
+  constructor() {
+
+  }
+
+  public pick(weapon: number): void {
+    // return immediately when still loading. You don't want
+    // the user to spam the button.
+    if (this.loading) return;
+    this.loading = true;
+    this.playerSelected = weapon;
+
+    setTimeout(() => {
+      this.loading = false;
+      const randomNum = Math.floor(Math.random() * 3);
+      this.enemySelected = randomNum;
+      this.checkResult();
+      this.isResultShow = true;
+    }, Math.floor(Math.random() * 500) + 900);
+  }
+
+  public reset(): void {
+    this.scores = [0, 0];
+  }
+
+  private checkResult(): void {
+    const playerPick = this.playerSelected;
+    const enemyPick = this.enemySelected;
+    if (playerPick == enemyPick) {
+      this.theResult = 2;
+    }
+    else if ((playerPick - enemyPick + 3) % 3 == 1) {
+      // YOU WIN
+      this.theResult = 0;
+      this.scores[0] = this.scores[0] + 1;
+    }
+    else {
+      // YOU LOSE
+      this.theResult = 1;
+      this.scores[1] = this.scores[1] + 1;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,41 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    AngularFontAwesomeModule
   ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
