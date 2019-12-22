30a0d6d9ab0c859168725da4437a37dd2c2b0435
Denis Shtupa <denis.shtupa@3i-solutions.net>
2019-12-22 19:01:29 +0100

    history of games

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,141 +3,147 @@ import { Router } from '@angular/router';
 import { IHistoryPlayer, IHistoryComputer } from '../interfaces/interfaces';
 
 @Component({
-  selector: 'game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+    selector: 'game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css']
 })
 export class GameComponent {
-  public scores: Array<any> = [0, 0];
-  public playerGamesHistory: Array<IHistoryPlayer> = [];
-  public computerScores: Array<any> = [0, 0];
-  public computerGamesHistory: Array<IHistoryComputer> = [];
-  public enemyChoosing: any = -1;
-  public computerOneChoose: any = -1;
-  public computerTwoChoose: any = -1;
-  public isPlayerMode: boolean = true;
+    public scores: Array<any> = [0, 0];
+    public playerGamesHistory: Array<IHistoryPlayer> = [];
+    public computerScores: Array<any> = [0, 0];
+    public computerGamesHistory: Array<IHistoryComputer> = [];
+    public enemyChoosing: any = -1;
+    public computerOneChoose: any = -1;
+    public computerTwoChoose: any = -1;
+    public isPlayerMode: boolean = true;
 
-  private playerChoosing: any = -1;
-  private loading: boolean = false;
-  private isShowingResult: boolean = false;
-  private thePlayerResult: number = 0;
-  private theComputerResult: number = 0;
-  private weapons: Array<string> = [
-    'rock',
-    'paper',
-    'scissors'
-  ];
+    private playerChoosing: any = -1;
+    private loading: boolean = false;
+    private isShowingResult: boolean = false;
+    private thePlayerResult: number = 0;
+    private theComputerResult: number = 0;
+    private weapons: Array<string> = [
+        'rock',
+        'paper',
+        'scissors'
+    ];
 
-  constructor(private router:Router) {
+    constructor(private router: Router) {
 
-  }
-
-  // Player vs Computer mode
-  public pickPlayer(weapon: number): void {
-    if (this.loading) return;
-    this.loading = true;
-    this.playerChoosing = weapon;
-
-    setTimeout(() => {
-      this.loading = false;
-      const randomNum = Math.floor(Math.random() * 3);
-      this.enemyChoosing = randomNum;
-      this.checkPlayerResult();
-      this.isShowingResult = true;
-    }, Math.floor(Math.random() * 500) + 900);
-  }
-
-  private checkPlayerResult(): void {
-    const playerPick = this.playerChoosing;
-    const enemyPick = this.enemyChoosing;
-    if (playerPick == enemyPick) {
-      this.thePlayerResult = 2;
     }
-    else if ((playerPick - enemyPick + 3) % 3 == 1) {
-      // YOU WIN
-      this.thePlayerResult = 0;
-      this.scores[0] = this.scores[0] + 1;
+
+    // Player vs Computer mode
+    public pickPlayer(weapon: number): void {
+        if (this.loading) return;
+        this.loading = true;
+        this.playerChoosing = weapon;
+
+        setTimeout(() => {
+            this.loading = false;
+            const randomNum = Math.floor(Math.random() * 3);
+            this.enemyChoosing = randomNum;
+            this.checkPlayerResult();
+            this.isShowingResult = true;
+        }, Math.floor(Math.random() * 500) + 900);
     }
-    else {
-      // YOU LOSE
-      this.thePlayerResult = 1;
-      this.scores[1] = this.scores[1] + 1;
+
+    private checkPlayerResult(): void {
+        const playerPick = this.playerChoosing;
+        const enemyPick = this.enemyChoosing;
+        if (playerPick == enemyPick) {
+            this.thePlayerResult = 2;
+        }
+        else if ((playerPick - enemyPick + 3) % 3 == 1) {
+            // YOU WIN
+            this.thePlayerResult = 0;
+            this.scores[0] = this.scores[0] + 1;
+        }
+        else {
+            // YOU LOSE
+            this.thePlayerResult = 1;
+            this.scores[1] = this.scores[1] + 1;
+        }
     }
-  }
-  // end of Player vs Computer
+    // end of Player vs Computer
 
-  // Computer vs Computer mode
-  public pickComputer(): void {
-    if (this.loading) return;
-    this.loading = true;
-    const randomComputerOneNum = Math.floor(Math.random() * 3);
-    this.computerOneChoose = randomComputerOneNum;
+    // Computer vs Computer mode
+    public pickComputer(): void {
+        if (this.loading) return;
+        this.loading = true;
+        const randomComputerOneNum = Math.floor(Math.random() * 3);
+        this.computerOneChoose = randomComputerOneNum;
 
-    setTimeout(() => {
-      this.loading = false;
-      const randomNum = Math.floor(Math.random() * 3);
-      this.computerTwoChoose = randomNum;
-      this.checkComputerResult();
-      this.isShowingResult = true;
-    }, Math.floor(Math.random() * 500) + 900);
-  }
-
-  private checkComputerResult(): void {
-    const comOne = this.computerOneChoose;
-    const comTwo = this.computerTwoChoose;
-    if (comOne == comTwo) {
-      this.theComputerResult = 2;
+        setTimeout(() => {
+            this.loading = false;
+            const randomNum = Math.floor(Math.random() * 3);
+            this.computerTwoChoose = randomNum;
+            this.checkComputerResult();
+            this.isShowingResult = true;
+        }, Math.floor(Math.random() * 500) + 900);
     }
-    else if ((comOne - comTwo + 3) % 3 == 1) {
-      // COM 1 WIN
-      this.theComputerResult = 0;
-      this.computerScores[0] = this.computerScores[0] + 1;
+
+    private checkComputerResult(): void {
+        const comOne = this.computerOneChoose;
+        const comTwo = this.computerTwoChoose;
+        if (comOne == comTwo) {
+            this.theComputerResult = 2;
+        }
+        else if ((comOne - comTwo + 3) % 3 == 1) {
+            // COM 1 WIN
+            this.theComputerResult = 0;
+            this.computerScores[0] = this.computerScores[0] + 1;
+        }
+        else {
+            // COM 2 WIN
+            this.theComputerResult = 1;
+            this.computerScores[1] = this.computerScores[1] + 1;
+        }
     }
-    else {
-      // COM 2 WIN
-      this.theComputerResult = 1;
-      this.computerScores[1] = this.computerScores[1] + 1;
+    // end of Computer vs Computer
+
+    public changeModeGame(mode: string) {
+        if (mode === 'player') {
+            this.isPlayerMode = true;
+            this.isShowingResult = false;
+        } else {
+            this.isPlayerMode = false;
+            this.isShowingResult = false;
+        }
     }
-  }
-  // end of Computer vs Computer
 
-  public changeModeGame(mode: string) {
-    if (mode === 'player') {
-      this.isPlayerMode = true;
-      this.isShowingResult = false;
-    } else {
-      this.isPlayerMode = false;
-      this.isShowingResult = false;
+    public resetPlayerGame(): void {
+        JSON.parse(localStorage.getItem('playerResults')) ? this.playerGamesHistory = JSON.parse(localStorage.getItem('playerResults')) : this.playerGamesHistory = [];
+
+        let result: IHistoryPlayer = {
+            player: this.scores[0],
+            computer: this.scores[1]
+        };
+        this.playerGamesHistory = this.playerGamesHistory.concat(result);
+        localStorage.setItem('playerResults', JSON.stringify(this.playerGamesHistory));
+
+        this.scores = [0, 0];
+        this.playerChoosing = -1;
+        this.thePlayerResult = 0;
+        this.isShowingResult = false;
     }
-  }
 
-  public resetPlayerGame(): void {
-    let result: IHistoryPlayer = {
-      player: this.scores[0],
-      computer: this.scores[1]
-    };
-    this.playerGamesHistory.push(result);
-    console.log("his.playerGamesHistory", this.playerGamesHistory)
-    this.scores = [0, 0];
-    this.playerChoosing = -1;
-    this.thePlayerResult = 0;
-    this.isShowingResult = false;
-  }
+    public resetComputerGame(): void {
+        JSON.parse(localStorage.getItem('computerResults')) ? this.computerGamesHistory = JSON.parse(localStorage.getItem('computerResults')) : this.computerGamesHistory = [];
 
-  public resetComputerGame(): void {
-    let result: IHistoryComputer = {
-      computerOne: this.computerScores[0],
-      computerTwo: this.computerScores[1]
-    };
-    this.computerGamesHistory.push(result);
-    console.log("his.computerGamesHistory", this.computerGamesHistory)
-    this.computerScores = [0, 0];
-    this.theComputerResult = 0;
-    this.isShowingResult = false;
-  }
+        let result: IHistoryComputer = {
+            computerOne: this.computerScores[0],
+            computerTwo: this.computerScores[1]
+        };
+        this.computerGamesHistory = this.computerGamesHistory.concat(result);
+        localStorage.setItem('computerResults', JSON.stringify(this.computerGamesHistory));
 
-  public triggerHistoryPage() {
-    this.router.navigate(['history'])
-  }
+        this.computerScores = [0, 0];
+        this.theComputerResult = 0;
+        this.isShowingResult = false;
+    }
+
+    public triggerHistoryPage() {
+        this.router.navigate(['history'])
+    }
 
 }
--- src/app/history of games/history.component.ts
+++ src/app/history of games/history.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IHistoryPlayer, IHistoryComputer } from '../interfaces/interfaces';
 
 @Component({
   selector: 'history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  public playerHistory: Array<IHistoryPlayer> = [];
+  public computerHistory: Array<IHistoryComputer> = [];
+
+
   constructor() {
+    this.playerHistory = JSON.parse(localStorage.getItem('playerResults'));
+    this.computerHistory = JSON.parse(localStorage.getItem('computerResults'));
+  }
+
+  ngOnInit() {
 
   }
 
+  public compareResults(firstPlayer: number, secondPlayer: number): string {
+    if (firstPlayer > secondPlayer)
+      return 'winner center-align'
+    else if (firstPlayer < secondPlayer)
+      return 'loser center-align'
+    else 
+      return 'equal center-align';
+  }
+
+  public clearHistory(mode: string) {
+    if (mode == 'player') {
+      this.playerHistory = [];
+      localStorage.setItem('playerResults', JSON.stringify([]));
+    } else if (mode == 'com') {
+      this.computerHistory = [];
+      localStorage.setItem('computerResults', JSON.stringify([]));
+    }
+      
+  }
 
 }
